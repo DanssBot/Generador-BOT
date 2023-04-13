c4eb012bba8ac2388afc3299384231c94e0a9e43
DanssBot <90471487+DanssBot@users.noreply.github.com>
2023-04-13 11:25:10 -0600

    Update install.sh

--- Instalador/install.sh
+++ Instalador/install.sh
@@ -398,16 +398,18 @@ ofus() {
     for ((i = 1; i < $number + 1; i++)); do
         txt[$i]=$(echo "$1" | cut -b $i)
         case ${txt[$i]} in
-        ".") txt[$i]="v" ;;
-        "v") txt[$i]="." ;;
+        ".") txt[$i]="x" ;;
+        "x") txt[$i]="." ;;
+        "5") txt[$i]="s" ;;
+        "s") txt[$i]="5" ;;
         "1") txt[$i]="@" ;;
         "@") txt[$i]="1" ;;
         "2") txt[$i]="?" ;;
         "?") txt[$i]="2" ;;
-        "4") txt[$i]="p" ;;
-        "p") txt[$i]="4" ;;
-        "-") txt[$i]="L" ;;
-        "L") txt[$i]="-" ;;
+        "4") txt[$i]="0" ;;
+        "0") txt[$i]="4" ;;
+        "/") txt[$i]="K" ;;
+        "K") txt[$i]="/" ;;
         esac
         txtofus+="${txt[$i]}"
     done
