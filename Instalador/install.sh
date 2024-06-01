c83c68da311a63eddb36c4dade0a7122362af564
DanssBot <90471487+DanssBot@users.noreply.github.com>
2024-06-01 16:15:02 -0600

    Update install.sh

--- Instalador/install.sh
+++ Instalador/install.sh
@@ -388,7 +388,7 @@ fun_filez() {
     fup="$HOME/update"
     echo "$1" >>$HOME/files.log
 
-    wget -O /bin/http-server.sh https://raw.githubusercontent.com/DanssBot/Generador-BOT/main/Bot/http-server.py  && chmod +x /bin/http-server.sh
+    wget -O /bin/http-server.sh https://raw.githubusercontent.com/NetVPS/Generador-BOT/main/Bot/http-server.py  && chmod +x /bin/http-server.sh
     [[ -e $1 ]] && mv -f ${fup}/$1 /etc/SCRIPT/$1
 }
 
